ece4b06db75d383906e0ca73fa9c188cf335ef1e
Fernando Almeida <fernando@fernandoalmeida.net>
2020-01-09 02:06:16 -0300

    [haskell][hello] Get name interactively

--- haskell/hello/hello.hs
+++ haskell/hello/hello.hs
@@ -1,7 +1,7 @@
 module Main where
-import System.Environment
 
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn ("Hello, " ++ args !! 0)
+  putStrLn ("Enter a name")
+  name <- getLine
+  putStrLn ("Hello, " ++ name)
